33634847f7e8ba54118ae37ab9d52f8953887322
Qianna <zhq9669@gmail.com>
2020-08-19 20:05:05 +0800

    none

---
+++ openselfsup/datasets/data_sources/marvel.py
@@ -0,0 +1,20 @@
+from ..registry import DATASOURCES
+from .image_list import ImageList
+from PIL import Image
+
+
+@DATASOURCES.register_module
+class Marvel(object):
+
+    def __init__(self, list_file):
+        with open(list_file, 'r') as f:
+            lines = f.readlines()
+        self.fns = [l.split(',')[-1] for l in lines]
+
+    def get_length(self):
+        return len(self.fns)
+
+    def get_sample(self, idx):
+        img = Image.open(self.fns[idx])
+        img = img.convert('RGB')
+        return img
