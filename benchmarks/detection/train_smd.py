0497a42e8f3b1e3de1d176ab96845c9c847db258
Qianna <zhq9669@gmail.com>
2020-08-20 23:11:26 +0800

    none

--- benchmarks/detection/train_smd.py
+++ benchmarks/detection/train_smd.py
@@ -19,10 +19,6 @@ register_coco_instances("smd_val", {}, "/root/data/zq/data/SMD/annotations/Train
                         "/root/data/zq/data/SMD/train")
 register_coco_instances("smd_test", {}, "/root/data/zq/data/SMD/annotations/Test/SMD_VIS_skip_2.json",
                         "/root/data/zq/data/SMD/test")
-MetadataCatalog.get("smd_train").thing_classes = ["object"]
-MetadataCatalog.get("smd_val").thing_classes = ["object"]
-MetadataCatalog.get("smd_test").thing_classes = ["object"]
-
 
 @ROI_HEADS_REGISTRY.register()
 class Res5ROIHeadsExtraNorm(Res5ROIHeads):
