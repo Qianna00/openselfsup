5b8f11e660cec645bae56fc9fff6e9efc3cee5a0
Qianna <zhq9669@gmail.com>
2020-08-27 20:03:05 +0800

    none

--- configs/selfsup/moco/r50_v1_marvel.py
+++ configs/selfsup/moco/r50_v1_marvel.py
@@ -39,8 +39,8 @@ train_pipeline = [
     dict(type='Normalize', **img_norm_cfg),
 ]
 data = dict(
-    imgs_per_gpu=128,  # total 32*8=256
-    workers_per_gpu=8,
+    imgs_per_gpu=256,  # total 32*8=256
+    workers_per_gpu=16,
     drop_last=True,
     train=dict(
         type=dataset_type,
