85d9f398b170bb53100fa4033bd8f5dc3a198c1f
Qianna <zhq9669@gmail.com>
2020-09-11 10:55:53 +0800

    none

--- benchmarks/detection/train_smd_10c.py
+++ benchmarks/detection/train_smd_10c.py
@@ -55,8 +55,8 @@ def setup(args):
     cfg.merge_from_file(args.config_file)
     cfg.merge_from_list(args.opts)
     cfg.OUTPUT_DIR = "/root/data/zq/unsup_det/imagenet_pretrain_det_10c_focal"
-    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 10
-    # cfg.MODEL.RETINANET.NUM_CLASSES = 10
+    # cfg.MODEL.ROI_HEADS.NUM_CLASSES = 10
+    cfg.MODEL.RETINANET.NUM_CLASSES = 10
     cfg.freeze()
     default_setup(cfg, args)
     return cfg
