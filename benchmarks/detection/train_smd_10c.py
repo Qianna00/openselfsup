b2295996114b83e2468154779b585f9a0d9bfe10
Qianna <zhq9669@gmail.com>
2020-09-20 09:53:21 +0800

    none

--- benchmarks/detection/train_smd_10c.py
+++ benchmarks/detection/train_smd_10c.py
@@ -54,10 +54,10 @@ def setup(args):
     cfg = get_cfg()
     cfg.merge_from_file(args.config_file)
     cfg.merge_from_list(args.opts)
-    cfg.OUTPUT_DIR = "/root/data/zq/smd_det/marvel_finetuned_det_7c/faster_rcnn"
-    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 7
-    # cfg.MODEL.RETINANET.NUM_CLASSES = 10
-    # cfg.MODEL.RETINANET.CB_LOSS_BETA = 0.99
+    cfg.OUTPUT_DIR = "/root/data/zq/smd_det/marvel_finetuned_det_7c/retinanet"
+    # cfg.MODEL.ROI_HEADS.NUM_CLASSES = 7
+    cfg.MODEL.RETINANET.NUM_CLASSES = 10
+    cfg.MODEL.RETINANET.CB_LOSS_BETA = 0.99
     cfg.freeze()
     default_setup(cfg, args)
     return cfg
