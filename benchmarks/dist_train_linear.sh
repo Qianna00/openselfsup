4b90306b9dcb38a9b3a93887255cb8b0c4c71a27
Qianna <zhq9669@gmail.com>
2020-10-13 14:59:40 +0800

    none

--- benchmarks/dist_train_linear.sh
+++ benchmarks/dist_train_linear.sh
@@ -7,7 +7,7 @@ CFG=$1 # use cfgs under "configs/benchmarks/linear_classification/"
 # PRETRAIN=$2
 PY_ARGS=${@:2} # --resume_from --deterministic
 GPUS=2 # When changing GPUS, please also change imgs_per_gpu in the config file accordingly to ensure the total batch size is 256.
-PORT=${PORT:-29500}
+PORT=${PORT:2020}
 
 # if [ "$CFG" == "" ] || [ "$PRETRAIN" == "" ]; then
     # echo "ERROR: Missing arguments."
