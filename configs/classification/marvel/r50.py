53be35525d714c0a9b806fe4a01e2229825a89ad
Qianna <zhq9669@gmail.com>
2020-10-13 15:08:48 +0800

    none

--- configs/classification/marvel/r50.py
+++ configs/classification/marvel/r50.py
@@ -27,7 +27,7 @@ test_pipeline = [
     dict(type='Normalize', **img_norm_cfg),
 ]
 data = dict(
-    imgs_per_gpu=128,
+    imgs_per_gpu=32,
     workers_per_gpu=2,
     train=dict(
         type=dataset_type,
