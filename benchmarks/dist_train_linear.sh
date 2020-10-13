9344abdc73b808dcae90933a916625ddddcd0f36
Qianna <zhq9669@gmail.com>
2020-10-13 11:33:06 +0800

    none

--- benchmarks/dist_train_linear.sh
+++ benchmarks/dist_train_linear.sh
@@ -14,8 +14,7 @@ if [ "$CFG" == "" ] || [ "$PRETRAIN" == "" ]; then
     exit
 fi
 
-# WORK_DIR="$(echo ${CFG%.*} | sed -e "s/configs/work_dirs/g")/$(echo $PRETRAIN | rev | cut -d/ -f 1 | rev)"
-WORK_DIR = "/root/data/zq/marvel_cls/supervised_from_scratch"
+WORK_DIR="$(echo ${CFG%.*} | sed -e "s/configs/work_dirs/g")/$(echo $PRETRAIN | rev | cut -d/ -f 1 | rev)"
 
 # train
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
