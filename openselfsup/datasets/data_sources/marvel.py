5b189ab46b37729217b39bb2b37608208a7d674b
Qianna <zhq9669@gmail.com>
2020-10-14 09:30:14 +0800

    none

--- openselfsup/datasets/data_sources/marvel.py
+++ openselfsup/datasets/data_sources/marvel.py
@@ -9,13 +9,8 @@ class Marvel(object):
     def __init__(self, list_file):
         with open(list_file, 'r') as f:
             lines = f.readlines()
-        self.fns = []
-        self.labels = []
-        for l in lines:
-            fn = l.split(',')[-1][:-1]
-            label = l.split(',')[2]
-            self.fns.append(fn)
-            self.labels.append(label)
+        self.fns = [l.split(',')[-1][:-1] for l in lines]
+        self.labels = [l.split(',')[2] for l in lines]
 
     def get_length(self):
         return len(self.fns)
