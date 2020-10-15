b69cece67ce484af141f34c424174136b0f463e0
Qianna <zhq9669@gmail.com>
2020-10-15 22:37:59 +0800

    none

--- openselfsup/datasets/data_sources/marvel.py
+++ openselfsup/datasets/data_sources/marvel.py
@@ -9,7 +9,8 @@ class Marvel(object):
     def __init__(self, list_file):
         with open(list_file, 'r') as f:
             lines = f.readlines()
-        self.fns = [l.split(',')[-1][:-1] for l in lines]
+        self.fns = [l.split(',')[-1][:-1] if l.split(',')[-1] != "/root/data/zq/data/marvel/140k/W9_5/925860.jpg"
+                    else l.split(',')[-1] for l in lines]
         self.labels = [int(l.split(',')[2]) for l in lines]
 
     def get_length(self):
