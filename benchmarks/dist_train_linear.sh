d3ede490d2c0e39fcc70952a5b43d3a55d9f4fc5
Qianna <zhq9669@gmail.com>
2020-10-16 11:05:57 +0800

    none

--- benchmarks/dist_train_linear.sh
+++ benchmarks/dist_train_linear.sh
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env bash
 
 set -e
@@ -5,7 +6,7 @@ set -x
 
 CFG=$1 # use cfgs under "configs/benchmarks/linear_classification/"
 PRETRAIN=$2
-PY_ARGS=${@:2} # --resume_from --deterministic
+PY_ARGS=${@:3} # --resume_from --deterministic
 GPUS=4 # When changing GPUS, please also change imgs_per_gpu in the config file accordingly to ensure the total batch size is 256.
 PORT=${PORT:-2020}
 
@@ -17,6 +18,8 @@ fi
 WORK_DIR="$(echo ${CFG%.*} | sed -e "s/configs/work_dirs/g")/$(echo $PRETRAIN | rev | cut -d/ -f 1 | rev)"
 
 # train
+python tools/extract_backbone_weights.py /root/data/zq//marvel_cls/selfsup/epoch_50.pth \
+/root/data/zq/marvel_cls/selfsup/pretrained_res50.pth
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
     tools/train.py \
     $CFG \
