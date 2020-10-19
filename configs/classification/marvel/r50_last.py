9d239376164683ed9c2aca57eef81ac90751b6cc
Qianna <zhq9669@gmail.com>
2020-10-19 20:24:17 +0800

    none

--- configs/classification/marvel/r50_last.py
+++ configs/classification/marvel/r50_last.py
@@ -55,9 +55,9 @@ custom_hooks = [
         eval_param=dict(topk=(1,)))
 ]
 # optimizer
-optimizer = dict(type='SGD', lr=0.05, momentum=0.9, weight_decay=0.)
+optimizer = dict(type='SGD', lr=0.1, momentum=0.9, weight_decay=0.)
 # learning policy
-lr_config = dict(policy='step', step=[25, 40])
+lr_config = dict(policy='step', step=[20, 30])
 checkpoint_config = dict(interval=2)
 # runtime settings
-total_epochs = 50
+total_epochs = 35
