0a242f0e0bdda2d53226602dd0865efd59aa3160
Qianna <zhq9669@gmail.com>
2020-10-19 16:05:02 +0800

    none

--- benchmarks/dist_test_linear.sh
+++ benchmarks/dist_test_linear.sh
@@ -6,11 +6,12 @@ set -x
 CFG=$1 # use cfgs under "configs/benchmarks/linear_classification/"
 GPUS=$2
 CHECKPOINT=$3
+WORK_DIR=$4
 PORT=${PORT:-2001}
 
 
 # WORK_DIR="$(echo ${CFG%.*} | sed -e "s/configs/work_dirs/g")/$(echo $PRETRAIN | rev | cut -d/ -f 1 | rev)"
-WORK_DIR="$(dirname $CHECKPOINT)/"
+# WORK_DIR="$(dirname $CHECKPOINT)/"
 
 # test
 
