433990b447f7837c3d4f4b105c60c37b20e694a3
Qianna <zhq9669@gmail.com>
2020-10-31 16:53:55 +0800

    none

--- benchmarks/detection/train_smd_10c.py
+++ benchmarks/detection/train_smd_10c.py
@@ -55,9 +55,9 @@ def setup(args):
     cfg.merge_from_file(args.config_file)
     cfg.merge_from_list(args.opts)
     cfg.OUTPUT_DIR = "/root/data/zq/smd_det/marvel_finetuned_det_7c/cb_focal"
-    # cfg.MODEL.ROI_HEADS.NUM_CLASSES = 7
-    cfg.MODEL.RETINANET.NUM_CLASSES = 7
-    cfg.MODEL.RETINANET.CB_LOSS_BETA = 0.99
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 10
+    # cfg.MODEL.RETINANET.NUM_CLASSES = 10
+    # cfg.MODEL.RETINANET.CB_LOSS_BETA = 0.99
     cfg.freeze()
     default_setup(cfg, args)
     return cfg
