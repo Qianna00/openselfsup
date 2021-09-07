c2e32873ed43fad61559f1f3fc5e022f9ea23375
Qianna <892878159@qq.com>
2021-09-07 11:05:41 +0800

    none

--- openselfsup/models/detco.py
+++ openselfsup/models/detco.py
@@ -235,7 +235,7 @@ class DetCo(nn.Module):
             k_4 = nn.functional.normalize(self.encoder_k_necks[2](k_4), dim=1)
             k_5 = nn.functional.normalize(self.encoder_k_necks[3](k_5), dim=1)
 
-            p_k_2, p_k_3, p_k_4, p_k_5 = self.encoder_k(patch_k)
+            p_k_2, p_k_3, p_k_4, p_k_5 = self.backbone_k(patch_k)
             k_l_2 = nn.functional.normalize(
                 self.encoder_k_patch_necks[0](p_k_2.view(-1, 9 * p_k_2.size(1), p_k_2.size(2), p_k_2.size(3))), dim=1)
             k_l_3 = nn.functional.normalize(
